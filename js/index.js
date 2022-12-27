d8260b2733cbc2592ac881f96c67b74d3336bb2e
Dasha Panchenko <dashapanchenko@Dashas-MacBook-Pro.local>
2022-12-27 13:01:33 +0200

    module-01-lesson-02

---
+++ js/index.js
@@ -0,0 +1,208 @@
+// if, if else
+
+//console.log('Polly' || 'Mango');
+// let iam = "I'm";
+// console.log(iam);
+
+// console.log('Mange' && null || false);
+// let num = 10;
+
+// if (num < 8) {
+//     console.log(`${num} less than 8`);
+// } else {
+//     console.log(`${num} more than 8`);
+// }
+
+// let num = 6;
+
+// if (num < 8) {
+//     console.log(`${num} less than 8`);
+// } 
+
+// if (num === 6) {
+//     console.log('It is 6');
+// }
+
+// let num = 6;
+
+// if (num < 8) {
+//     console.log(`${num} less than 8`);
+// } else if (num === 6) {
+//     console.log('It is 6');
+// }
+
+// let age = 21;
+
+// if (age < 18) {
+//     console.log('You are a kid');
+// } else if (age > 65) {
+//     console.log('You are an adult');
+// } else {
+//     console.log('You are young');
+// }
+
+// let num = 6;
+
+// if ('') {
+//     let someText = 'text';
+
+//     console.log(`Hi, it is true - ${someText}`);
+// } else {
+//     console.log( 'It is false');
+// }
+
+// let testNumber = num < 8 ? 'Hello' : 'It is false';
+
+// console.log('' ? 'Hi, it is true' : 'It is false');
+
+// let myPass = 'testPass';
+
+// if (myPass === 'Daria') {
+//     console.log('Your pass is Daria');
+// } else if (myPass === '1999') {
+//     console.log('Your pass is 1999');
+// } else if (myPass === 'testPass') {
+//     console.log('Your pass is correct');
+// } else {
+//     console.log('You write another password');
+// }
+
+// let myPass = 'testPass';
+// let daria = 'Daria';
+
+// switch (myPass) {
+//     case daria:
+//         console.log('Your pass is Daria');
+//         break;
+//     case 1999:
+//         console.log('Your pass is 1999');
+//         break;
+//     case 'testPass':
+//         console.log('Your pass is correct');
+//         break;
+//     default:
+//         console.log('You write another password');
+// }
+
+// let num = 5;
+
+// switch (num + 3) {
+//     case 7: 
+//         console.log(`It is ok`);
+//         break;
+//     case 8: 
+//         console.log(`It is 8`);
+//         break;
+//     default: 
+//         console.log('it is false');
+// }
+
+// console.log(num > 3 ? `${num} more than 3` : 'it is false');
+
+
+// let num = 5;
+// const testNum = 10;
+
+// if (true) {
+//     var numVar = 3;
+//     numVar = 5;
+//     let newNum = 8;
+//     //const testNum = 10;
+
+//     //console.log('if statement -->', newNum);
+// }
+
+//console.log('var -->', numVar);
+// console.log('Global scope -->', num);
+//console.log('Block scope -->', newNum);
+//console.log('Global scope const -->', testNum);
+
+// while
+// let iterator = 0;
+
+// while (iterator < 5) {
+//     iterator += 1;
+//     // iterator = iterator + 1;
+//     console.log(`Hello, iterator = ${iterator}`);
+// }
+
+// let iterator = 0;
+
+// do {
+//     iterator += 1;
+
+//     console.log(`Hello, iterator = ${iterator}`);
+// } while (iterator < 5);
+
+// let num = 10;
+
+// for (let i = 0; i < 5; i++) {
+//     num += 20;
+//     console.log(num);
+// }
+
+// let num = 0;
+
+// for (let i = 0; i < 12; i++) {
+//     num += 5;
+//     console.log('num --> ', num);
+//     continue;
+
+//     console.log('i -->', i);
+// }
+
+// let animal = 'elephant';
+
+// console.log(animal.length);
+
+// for (let i = 0; i < animal.length; i++) {
+//     if (animal[i] === 'a') {
+//         break;
+//     }
+
+//     console.log(animal[i]);
+// }
+
+// let boxes = 5;
+
+// let ball;
+
+// for (let i = 0; i < 5; i++) {
+//     if (ball === 'white') {
+//         continue;
+//     }
+
+//     if (ball === 'green') {
+//         break;
+//     }
+    
+//     console.log(i);
+// }
+
+// let firstName = 'Daria';
+// let age = 21;
+// let isContains = false;
+
+// if (isContains || age > 18) {
+//     console.log('Welcome!');
+// }
+
+// && || !
+
+// != !== --> == === 
+// console.log(5 !== '5');
+
+// let amount = 100;
+// let myMoney = 10;
+// let age = 17;
+
+// if (!myMoney) {
+//     console.log('Welcome!');
+// }
+
+// if (1 || 2 && false || 0) {
+//     console.log('Welcome!');
+// }
+
+console.log(1 || 2 && false || 0);
+
